20dd2da11df5e180219b014acae3e01aed2d6327
Kyle Burnette <kyle@kyleburnette.com>
2021-03-13 12:56:25 -0800

    Rudimentary file upload

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
 from app import app
-from flask import render_template
+from app import watermark
+from flask import Flask, render_template, request, redirect, url_for
 import os
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            uploaded_file.save(uploaded_file.filename)
+
+        return redirect(url_for('index'))
+    return render_template('index.html')
