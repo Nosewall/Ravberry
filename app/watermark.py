fa32e598af8b1eba45dd1e30e7c11bfccfdcb0fb
Mike Lim <mslim96@gmail.com>
2021-03-13 12:36:07 -0800

    updates to watermark.py

--- app/watermark.py
+++ app/watermark.py
@@ -2,18 +2,37 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-def wm_text(in_im_path, out_im_path, text, pos):
-    photo = Image.open(in_im_path)
+def watermark_text(in_img_path, out_img_path, text, pos, size, color, font_name, resolution):
+    photo = Image.open(in_img_path)
 
     drawing = ImageDraw.Draw(photo)
 
-    color = 15
-
-    font = ImageFont.truetype("fonts/AgiliaItalic.ttf", 40)
+    font = ImageFont.truetype(font_name, size)
 
     drawing.text(pos, text, fill=color, font=font)
 
     photo.show()
+
+    photo.save(out_img_path)
+
+
+def watermark_img(in_img_path, out_img_path, img_to_watermark_path, pos, size, resolution)
+    base_image = Image.open(in_img_path)
+    watermark_image = Image.open(img_to_watermark_path)
+
+    watermark_image = watermark_image.resize((basewidth,hsize), Image.ANTIALIAS)
+
+    base_image.paste(watermark_image, pos)
+
+    base image = resolution(resolution, base_image)
+
+    base_image.show()
+
+    base_image.save(out_img_path)
+
+def resolution(res, photo)
+    
+
 
 if __name__ == '__main__':
     img = 'static/ravberry.png'
