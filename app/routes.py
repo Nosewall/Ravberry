58f077b0c1b5be2e5ca88b0609a54435037c859c
Kyle Burnette <kyle@kyleburnette.com>
2021-03-13 20:52:10 -0800

    merged rav's changes to routes

--- app/routes.py
+++ app/routes.py
@@ -6,17 +6,35 @@ import os
 
 uploads_dir = Path(os.path.abspath(os.getcwd()) + "/app/static/uploads")
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        uploaded_file = request.files['file']
-        if uploaded_file.filename != '':
+    if request.method == "POST":
+        uploaded_file = request.files["file"]
+        if uploaded_file.filename != "":
             filename = os.path.join(str(uploads_dir), uploaded_file.filename)
             uploaded_file.save(os.path.join(uploads_dir, uploaded_file.filename))
-            # watermark.watermark_text(str(filename), str(uploads_dir), "asdflasdkjfa", [50, 50])
-            return render_template('options.html', image_url = "../static/uploads/" + uploaded_file.filename)
-    return render_template('index.html')
+            return render_template(
+                "options.html", image_url="../static/uploads/" + uploaded_file.filename
+            )
+    return render_template("index.html")
 
-@app.route('/success', methods=['GET', 'POST'])
-def success():
-    return render_template('success.html')
+
+@app.route("/options", methods=["GET", "POST"])
+def options():
+    if request.method == "POST":
+        # water_mark = request.files["watermark"]
+        # text = request.form["text"]
+        # resolution = request.form["resolution"]
+        # font = request.form["fonts"]
+        if uploaded_file.filename[-3:] == "png" or uploaded_file.filename[-3:] == "gif":
+            print("transparency")
+            watermark.watermark_with_transparency(
+                str(filename), str(uploads_dir), " ", [50, 50]
+            )
+            return render_template(
+                "options.html", image_url="../static/uploads/" + uploaded_file.filename
+            )
+        else:
+            print("solid")
+            watermark.watermark_img(str(filename), str(uploads_dir), text, [50, 50])
