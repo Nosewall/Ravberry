ca730762a4bd830588602850338d6fcb83f31883
Nolan <nolannordwall@gmail.com>
2021-03-13 19:54:02 -0800

    Fixing merge conflicts

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-from app import app
-from app import watermark
-from pathlib import Path
-from flask import Flask, render_template, request, redirect, url_for
-import os
-
-uploads_dir = Path(os.path.abspath(os.getcwd()) + "/app/static/uploads")
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        uploaded_file = request.files['file']
-        if uploaded_file.filename != '':
-            filename = os.path.join(str(uploads_dir), uploaded_file.filename)
-            uploaded_file.save(os.path.join(uploads_dir, uploaded_file.filename))
-            watermark.watermark_text(str(filename), str(uploads_dir), "asdflasdkjfa", [50, 50])
-            return render_template('options.html', image_url = "../static/uploads/" + uploaded_file.filename)
-=======
 =======
 from app import app
 from app import watermark
@@ -35,5 +16,4 @@ def index():
             uploaded_file.save(os.path.join(uploads_dir, uploaded_file.filename))
             watermark.watermark_text(str(filename), str(uploads_dir), "asdflasdkjfa", [50, 50])
             return render_template('options.html', image_url = "../static/uploads/" + uploaded_file.filename)
->>>>>>> refs/remotes/origin/main
     return render_template('index.html')
--- app/watermark.py
+++ app/watermark.py
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-import PIL
-import os
-
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
-from pathlib import Path
-
-def watermark_text(in_img_path, out_img_path, text, pos):
-    photo = Image.open(in_img_path)
-    
-    drawing = ImageDraw.Draw(photo)
-
-    filename = Path(os.path.join(os.path.abspath(os.getcwd()) + "/app/static/fonts/AgiliaItalic.ttf"))
-
-    temp_color = (0,0,0)
-
-    temp_size = 40
-
-    temp_font = ImageFont.truetype(str(Path(filename)), temp_size)
-
-    drawing.text(pos, text, fill=temp_color, font=temp_font)
-
-    path = out_img_path
-    photo.save(path + "/test.png", "PNG")
-
-def watermark_img(in_img_path, out_img_path, img_to_watermark_path, pos, size, resolution):
-    base_image = Image.open(in_img_path)
-    watermark_image = Image.open(img_to_watermark_path)
-
-    watermark_image = watermark_image.resize((basewidth,hsize), Image.ANTIALIAS)
-
-    base_image.paste(watermark_image, pos)
-
-    resolution(resolution, in_img_path)
-
-    base_image.show()
-
-    base_image.save(out_img_path)
-
-# res is on a scale of 1-100 95 is optimal
-def resolution(res, path):
-    image = image.open(path)
-
-    split_path = path.splitext(path)
-
-    image.save(split_path[0] + "_new_res" + split_path[1], quality=res)
-
-    
-if __name__ == '__main__':
-    img = 'static/ravberry.png'
-    wm_text(img, 'imageWaterMarked.png', text='hi there its mike lim', pos=(100, 100))
-=======
 import PIL
 import os
 
@@ -143,4 +88,3 @@ def calc_cordinates(image, x_ratio, y_ratio):
 if __name__ == "__main__":
     img = "static/ravberry.png"
     wm_text(img, "imageWaterMarked.png", text="hi there its mike lim", pos=(100, 100))
->>>>>>> refs/remotes/origin/main
